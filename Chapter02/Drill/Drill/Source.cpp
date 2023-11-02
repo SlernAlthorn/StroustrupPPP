d9e0bcad66a87d5c44ffc75cdf652fd08b313fd3
SlernAlthorn <mathias.dujardin@outlook.com>
2023-11-02 10:04:44 +0100

    Chapter 2 done.

---
+++ Chapter02/Drill/Drill/Source.cpp
@@ -0,0 +1,9 @@
+#include "../../../std_lib_facilities.h"
+
+int main()							// C++ programs start  by executing the function main.
+{
+	cout << "Hello, World!\n";		// Output "Hello, World!".
+
+	keep_window_open();				// Wait for a character to be entered.
+	return 0;
+}
